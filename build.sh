84c8fab19e19b6f72b90544470d18d195e391fcd
minh0518 <rubics@naver.com>
2024-01-05 00:14:37 +0900

    zzzzzz2

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
+# 이 스크립트는 teamRepo의 내용을 output 디렉토리로 복사합니다.
 
-# "output" 디렉토리를 생성하거나 비웁니다.
-rm -rf output
-mkdir output
+# 현재 GitHub Actions 워크플로우가 작업하는 디렉토리는 이미 teamRepo를 체크아웃한 상태입니다.
 
-# "teamRepo" 리포지토리의 빌드 결과물을 "output" 디렉토리로 복사합니다.
-# 복사할 파일들을 지정합니다. 예를 들어, 빌드 결과물이 "build" 디렉토리에 있다고 가정합니다.
-# cp 명령에 -R (재귀적 복사), -u (더 최신인 경우에만 복사) 옵션을 사용합니다.
-cp -Ru teamRepo/build/* output/
+# output 디렉토리를 생성합니다.
+mkdir -p output
 
-# 이후의 작업...
+# teamRepo의 내용을 output 디렉토리로 복사합니다.
+# 이 때, .git 디렉토리나 다른 GitHub Actions에 의해 생성된 임시 파일들은 제외해야 합니다.
+cp -R ./* output/ --exclude=.git
