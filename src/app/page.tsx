acedaa959021d5c3608588501a536c535c219b96
minh0518 <rubics@naver.com>
2024-01-08 11:38:37 +0000

    sync-main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,5 @@ import Image from 'next/image';
 import styles from './page.module.css';
 
 export default function Home() {
-  return <h1>sync deploy test</h1>;
+  return <h1>sync deploy test-main</h1>;
 }
